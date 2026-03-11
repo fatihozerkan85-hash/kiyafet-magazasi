f0911e09bc2966caa8a3f57a9353b19e2848e25b
fatihozerkan85-hash <fatihozerkan85@gmail.com>
2026-03-11 11:45:58 +0300

    Web logo boyutu 1150x100px olarak güncellendi

--- web/src/App.js
+++ web/src/App.js
@@ -425,7 +425,7 @@ function App() {
           style={{ 
             cursor: 'pointer',
             backgroundImage: `url(/as.png?v=${Date.now()})`,
-            backgroundSize: '1200px 200px',
+            backgroundSize: '1150px 100px',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
             width: '100%',
